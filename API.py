efef0e71935903c47c0f28d1cf922b5962acdae3
AlexB-png <alexander.brankin.466@accesscreative.ac.uk>
2025-01-28 12:56:16 +0000

    images?

--- API.py
+++ API.py
@@ -1,14 +1,23 @@
 import requests
+import tkinter as tk
 
+window = tk.Tk()
+window.geometry("900x800")
+
+label = tk.Label(text="Pikachu")
+label.pack()
 
 url = "https://pokeapi.co/api/v2/pokemon"
 
-pokemon = input("What pokemon")
+#pokemon = input("What pokemon")
+pokemon = "pikachu"
 
 url = url + "/" + pokemon
 
 response = requests.get(url)
-print(response.status_code)
+
 
 articles = response.json().get("sprites", []).get("back_shiny", [])
 print(articles)
+
+window.mainloop()
