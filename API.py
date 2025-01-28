dc8bf6e4f10535bb84c45cdc5e51cb00b6b49413
AlexB-png <alexander.brankin.466@accesscreative.ac.uk>
2025-01-28 15:26:04 +0000

    johbuiuiuwg

--- API.py
+++ API.py
@@ -1,23 +1,26 @@
+from tkinter import *
 import requests
-import tkinter as tk
 
-window = tk.Tk()
-window.geometry("900x800")
+window = Tk()
 
-label = tk.Label(text="Pikachu")
-label.pack()
 
 url = "https://pokeapi.co/api/v2/pokemon"
 
-#pokemon = input("What pokemon")
+# pokemon = input("What pokemon")
 pokemon = "pikachu"
-
 url = url + "/" + pokemon
-
 response = requests.get(url)
 
 
+canvas = Canvas(window, width = 300, height = 300)
+canvas.pack()
+img = PhotoImage(file="25.png")
+canvas.create_image(50,50, anchor=NW, image=img)
+
 articles = response.json().get("sprites", []).get("back_shiny", [])
 print(articles)
+
+frame = Frame(window, width=500, height=500)
+
 
 window.mainloop()
